01cd2ec7b0563a8fc7553384332545e74a63b23e
Unihilator <pupsinatoriaroman@gmail.com>
2019-10-12 16:18:02 +0300

    - added osbb contract. Defined User structure. Defined Issues type. Added User adding logic

---
+++ diff/OSBB/contracts/osbb.sol
@@ -0,0 +1,63 @@
+pragma solidity >=0.4.21 <0.6.0;
+import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract OSBB is Ownable {
+    
+    // Resident of OSBB
+    struct User {
+        bytes32 Name;
+        uint Flat;
+        uint Floor;
+        address addr;
+    }
+    
+    User[] usersArray;
+    
+    Issue[] issues;
+    
+    enum StatusOfIssue {
+        newIssue,
+        inProgress,
+        done
+    }
+    
+    // TODO
+    function isUserResident (address userAddress) public view returns (bool) {
+        return true;
+    }
+    
+    function addUser (bytes32 name,
+        uint flat,
+        uint floor,
+        address addr) public onlyOwner  {
+            require(name.length > 0, "Incorrect Name");
+            require(addr != address(0), "Wrong Address");
+            
+            usersArray.push(User({Name : name, Flat : flat, Floor : floor, addr : addr}));
+    }
+
+    
+    struct Issue {
+        string Description; // Description Of Issue
+        StatusOfIssue Status; // Initial is "newIssue". OSBB admin updates it to inProgress and done. 
+        // After done the user sould verify it
+        bool DoneApprovedByUser; // User approves Issue after done
+        uint CreationDate; // Creation date of issue
+        uint DueDate; // Date when the issue supposed to be done(User updates it)
+        uint CriticalLevel; // How critical is the issue(user updates it on creation)
+    }
+
+
+    constructor() public {
+        // TODO: add resident Limit
+    }
+}
+
+/**
+Создание Юзера ОСББ.
+	Имя, квартира, этаж.
+Создание Issue.
+	Description, Status, Approved by user, Creation Date, Due date, Critical level(Int)
+Обновление статуса Issue Админом(new, in progress, Done).
+Обновление статуса Approved(User)(Bool).
+ */
